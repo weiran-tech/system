080451545e7d2ae6e76f3f8787a5a17ac9e667f9
duoli <zhaody901@gmail.com>
2026-01-28 21:38:51 +0800

    refactor(http): 废弃旧网页及H5控制器，指向新控制器
    - 新增 MobileController 类，标记为废弃并继承新版控制器
    - 新增 WebController 类，标记为废弃并继承新版控制器
    - 为两个控制器添加严格类型声明及命名空间声明
    - 保持旧接口兼容性，便于平滑迁移至新控制器

---
+++ src/Http/Request/Web/MobileController.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Weiran\System\Http\Request\Web;
+
+/**
+ * H5 控制器
+ * @deprecated 1.0
+ * @see        \Weiran\System\Http\Web\MobileController
+ */
+abstract class MobileController extends \Weiran\System\Http\Web\MobileController {}
---
+++ src/Http/Request/Web/WebController.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Weiran\System\Http\Request\Web;
+
+/**
+ * 网页入口
+ * @deprecated 1.0
+ * @see        \Weiran\System\Http\Web\WebController
+ */
+abstract class WebController extends \Weiran\System\Http\Web\WebController {}
